23fb6adc5dc435724c54df7eb0776ee9cc276f3e
mottycruz <motty.cruz@gmail.com>
2018-01-23 15:41:40 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,9 @@ node default {
 }
 node 'master1.sscs.ad' {
       include role::master_server
+      file { '/home/README':
+          ensure => file,
+          content => $fqdn,
 }
 node /^web/ {
    include role::app_server
